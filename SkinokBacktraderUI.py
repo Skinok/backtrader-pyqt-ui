99ab9a50458bfd13a590e3061af04291d77d608f
mole <sleepmole@163.com>
2023-05-02 21:13:02 +0800

    fix import time delta total_seconds() issue

--- SkinokBacktraderUI.py
+++ SkinokBacktraderUI.py
@@ -182,21 +182,21 @@ class SkinokBacktraderUI:
         if len(df.index) > 2:
             dtDiff = df.index[1] - df.index[0]
 
-            if dtDiff.seconds == 60:
+            if dtDiff.total_seconds() == 60:
                 return "M1"
-            elif dtDiff.seconds == 300:
+            elif dtDiff.total_seconds() == 300:
                 return "M5"
-            elif dtDiff.seconds == 900:
+            elif dtDiff.total_seconds() == 900:
                 return "M15"
-            elif dtDiff.seconds == 1800:
+            elif dtDiff.total_seconds() == 1800:
                 return "M30"
-            elif dtDiff.seconds == 3600:
+            elif dtDiff.total_seconds() == 3600:
                 return "H1"
-            elif dtDiff.seconds == 14400:
+            elif dtDiff.total_seconds() == 14400:
                 return "H4"
-            elif dtDiff.seconds == 86400:
+            elif dtDiff.total_seconds() == 86400:
                 return "D"
-            elif dtDiff.seconds == 604800:
+            elif dtDiff.total_seconds() == 604800:
                 return "W"
 
         pass
