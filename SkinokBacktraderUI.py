83d5946098e0b906356fc206b605cb78ac3de7b7
michael xu <michaelxu88@163.com>
2023-05-10 09:05:00 +0800

    pandas 1.43 or 2.0.1

--- SkinokBacktraderUI.py
+++ SkinokBacktraderUI.py
@@ -112,22 +112,24 @@ class SkinokBacktraderUI:
 
             # Python contains
             if not dataPath in self.dataframes: 
-                # self.dataframes[fileName] = pd.read_csv(dataPath, 
-                #                                     sep=separator, 
-                #                                     parse_dates=[0], 
-                #                                     date_parser=lambda x: pd.to_datetime(x, format=datetimeFormat), 
-                #                                     skiprows=0, 
-                #                                     header=0, 
-                #                                     names=["Time", "Open", "High", "Low", "Close", "Volume"],
-                #                                     index_col=0)
-                self.dataframes[fileName] = pd.read_csv(dataPath, 
-                                                    sep=separator, 
-                                                    parse_dates=[0], 
-                                                    date_format=datetimeFormat, 
-                                                    skiprows=0, 
-                                                    header=0, 
-                                                    names=["Time", "Open", "High", "Low", "Close", "Volume"],
-                                                    index_col=0)
+                if pd.__version__=='1.4.3':
+                    self.dataframes[fileName] = pd.read_csv(dataPath, 
+                                                        sep=separator, 
+                                                        parse_dates=[0], 
+                                                        date_parser=lambda x: pd.to_datetime(x, format=datetimeFormat), 
+                                                        skiprows=0, 
+                                                        header=0, 
+                                                        names=["Time", "Open", "High", "Low", "Close", "Volume"],
+                                                        index_col=0)
+                else:
+                    self.dataframes[fileName] = pd.read_csv(dataPath, 
+                                                        sep=separator, 
+                                                        parse_dates=[0], 
+                                                        date_format=datetimeFormat, 
+                                                        skiprows=0, 
+                                                        header=0, 
+                                                        names=["Time", "Open", "High", "Low", "Close", "Volume"],
+                                                        index_col=0)
 
         except ValueError as err:
             return False, "ValueError error:" + str(err)
